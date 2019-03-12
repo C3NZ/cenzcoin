ebe4722275221a1cc11968eb94132616cf9d8ac1
C3NZ <cenz@cenz.io>
2019-03-12 02:29:18 -0700

    [feat] create a block class to represent blocks on the bc

---
+++ block.py
@@ -0,0 +1,13 @@
+'''
+    Block module for blocks that are stored on the blockchain
+'''
+from time import time
+
+
+class Block:
+    def __init__(self, index, previous_hash, transactions, proof, time=time()):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.proof = proof
+        self.timestamp = time
