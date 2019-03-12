9e44c54d52c8dbcfb91d07f47c02bc527b9eea9b
C3NZ <cenz@cenz.io>
2019-03-12 04:39:49 -0700

    [fix] instantiate verifier to work within the blockchain module

--- blockchain.py
+++ blockchain.py
@@ -11,6 +11,7 @@ from hash_util import hash_string_256, hash_block
 from files import save_data, load_data
 from block import Block
 from transaction import Transaction
+from verification import Verification
 
 # Mining reward for users mining blocks
 MINING_REWARD = 10
@@ -21,6 +22,8 @@ owner = 'cenz'
 
 # Load the block (From either json or binary )
 blockchain, open_transactions = load_data(from_json=True)
+
+verifier = Verification()
 
 def proof_of_work():
     '''
@@ -34,28 +37,10 @@ def proof_of_work():
     last_hash = hash_block(last_block)
     proof = 0
 
-    while not valid_proof(open_transactions, last_hash, proof):
+    while not verifier.valid_proof(open_transactions, last_hash, proof):
         proof += 1
 
     return proof
-
-def valid_proof(transactions, last_hash, proof):
-    '''
-        Check to see if the current proof is valid
-
-        Arguments:
-            :transactions: the list of transactions on the block
-            :last_hash: the hash of the previous block
-            :proof: the proof number used for attempting to generate a valid hash
-
-        Returns:
-            True if the guessed hash has two leading 0s, False otherwise
-    '''
-    hashable_txs = [tx.to_ordered_dict() for tx in transactions]
-    guess = (str(hashable_txs) + str(last_hash) + str(proof)).encode()
-    guessed_hash = hash_string_256(guess)
-
-    return guessed_hash[:2] == '00'
 
 
 def sum_transactions(tx_sum, txs):
@@ -112,30 +97,6 @@ def get_last_blockchain_value():
     else:
         return blockchain[-1]
 
-def verify_transaction(transaction):
-    '''
-        Veryify that the participant has a high enough balance in order to complete
-        a transaction
-
-        Arguments:
-            :transacation: The transaction that we're trying to verify
-
-        Returns:
-            True if the sender balance is greater than the tx amount and that
-            the tx amount is greater than 0
-    '''
-    sender_balance = get_balance(transaction.sender)
-
-    return sender_balance >= transaction.amount and transaction.amount > 0
-
-def verify_transactions():
-    '''
-        Validate that all open transactions within the open transactions
-
-        Returns:
-            True if all transactions are valid, False otherwise.
-    '''
-    return all(verify_transaction(tx) for tx in open_transactions)
 
 def add_transaction(sender, recipient, amount=1.0):
     '''
@@ -157,7 +118,7 @@ def add_transaction(sender, recipient, amount=1.0):
     transaction = Transaction(sender, recipient, amount)
     # If the transaction is legitimate, add it to the open transactions list and
     # keep track of participants
-    if verify_transaction(transaction):
+    if verifier.verify_transaction(transaction, get_balance):
         open_transactions.append(transaction)
         return True
 
@@ -226,34 +187,6 @@ def print_blockchain_elements():
     else:
         print('-' * 20)
 
-def verify_chain():
-    '''
-        Verify the current blockchain
-
-        Returns:
-            True if the blockchain is valid, False otherwise
-    '''
-
-    # Enumerate the blockchain in order to retrieve the current block & it's index
-    for index, block in enumerate(blockchain):
-        if index == 0:
-            continue
-
-        # Ensure the current blocks previous hash matches the hash of the previous block
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            print('The previous hash doesnt match the hash of the block on the blockchain ')
-            return False
-
-        #Select every part of the list except for the last element 
-        # in the transactions (the reward transaction) because it is not part of the proof of work calculation
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print('Proof of work is invalid')
-            return False
-
-    return True
-
-
-
 def main():
     '''
         Entry function for the blockchain to operate
@@ -283,7 +216,7 @@ def main():
         elif user_choice == '3':
             print_blockchain_elements()
         elif user_choice == '4':
-            if verify_transactions():
+            if verifier.verify_transactions(open_transactions, get_balance):
                 print('all open transactions are currently valid')
             else:
                 print('There are invalid transactions')
@@ -294,7 +227,7 @@ def main():
         print(f'Balance of {owner}: {get_balance(owner):6.2f}')
 
         # Verify the blockchain over every action
-        if not verify_chain():
+        if not verifier.verify_chain(blockchain):
             print_blockchain_elements()
             print('Invalid blockchain!')
             break
