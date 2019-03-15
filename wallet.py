94e9a6772e38e12bc1b5b59a0e02644788211af4
C3NZ <cenz@cenz.io>
2019-03-14 22:22:22 -0700

    [feat] add save/load functionality for wallets. Also add a sign_transaction function for signing off on transactions

--- wallet.py
+++ wallet.py
@@ -1,7 +1,10 @@
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 from Crypto import Random 
 import binascii
 
+from util.files import save_keys, load_keys
 
 class Wallet:
     def __init__(self):
@@ -11,7 +14,28 @@ class Wallet:
     def create_keys(self):
         self.private_key, self.public_key = self.generate_keys()
 
+    def save_keys(self):
+        '''
+            Save the wallets private and public keys 
+        '''
+
+        # Make sure there are keys to save 
+        if self.private_key and self.public_key:
+            save_keys(self.private_key, self.public_key)
+        else:
+            print('You need both a private and public key to save it to a file')
+
+    def load_keys(self):
+        self.private_key, self.public_key = load_keys()
+
+        if not self.private_key or not self.public_key:
+            print('Creating a new pair of keys')
+            self.create_keys()
+
     def generate_keys(self):
+        '''
+            Generate a new pair of 1024 bit RSA keys.
+        '''
         # Generate a 1024 bit rsa key object using Crypto.Random 
         # for a more secure random int generation algorithim
         rsa_obj = RSA.generate(1024, Random.new().read)
@@ -27,3 +51,30 @@ class Wallet:
         public_key_string = binascii.hexlify(public_key).decode('ascii')
 
         return (private_key_string, public_key_string)
+
+    def sign_transaction(self, sender, recipient, amount):
+        '''
+            Sign transactions that are authorized with this wallet for good security measures
+
+            Parameters:
+                :sender: (Yourself in this instance)
+                :recipient: The recipients wallet
+                :amount: the amount being sent to the recipient
+        '''
+        # Convert the users private key back into a binary encoded byte string
+        binary_key = binascii.unhexlify(self.private_key)
+
+        # With the users private key, create a new object named signer to authenticate a message with their
+        # private key
+        signer = PKCS1_v1_5.new(RSA.importKey(binary_key))
+
+        # create a sha256 hash of the current transaction as the payload to be signed
+        payload = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
+        # Sign the payload as proof that this transaction was initiated by th
+        binary_signature = signer.sign(payload)
+
+        # Convert the binary encoded signature back into ascii
+        str_signature = binascii.hexlify(binary_signature).decode('ascii')
+
+        return str_signature
+
