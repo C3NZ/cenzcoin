7cbab35d211fe3f7170174b1eab21ce586558b96
C3NZ <cenz@cenz.io>
2019-03-16 15:53:53 -0700

    [feat] add enpoint for viewing a copy of the blockchain

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
+from blockchain import Blockchain
 from wallet import Wallet
 
 # Setup server
@@ -9,10 +10,28 @@ CORS(app)
 
 # Setup wallet
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 
 @app.route('/', methods=['GET'])
 def get_root():
+    '''
+        Test route
+    '''
     return 'Initial setup!'
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    '''
+        Get a snapshot of the current chain
+
+        Status codes & Returns:
+           :200: returns a snapshot of the chain in json 
+    '''
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for block in dict_chain:
+        block['transactions'] = [transaction.__dict__ for transaction in block['transactions']]
+    return (jsonify(dict_chain), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
